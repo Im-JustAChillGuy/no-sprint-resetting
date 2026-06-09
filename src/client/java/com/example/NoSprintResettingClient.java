1e42e20a1c5c227f50839ff5e5dcc2d6e746a6b6
Im-JustAChillGuy <justforfunbruh1234@gmail.com>
2026-06-09 16:21:56 +0530

    Update NoSprintResettingClient.java

--- src/client/java/com/example/NoSprintResettingClient.java
+++ src/client/java/com/example/NoSprintResettingClient.java
@@ -6,6 +6,8 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 public class NoSprintResettingClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        NoSprintResettingConfig.load();
+
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (NoSprintResettingConfig.enabled && client.player != null) {
                 boolean sneaking = client.player.isShiftKeyDown();
