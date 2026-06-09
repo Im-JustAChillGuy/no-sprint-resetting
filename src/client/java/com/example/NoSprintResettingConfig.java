f81a6efa676262085af673fe1a2a738c273b5869
Im-JustAChillGuy <justforfunbruh1234@gmail.com>
2026-06-09 16:17:07 +0530

    Update NoSprintResettingConfig.java

--- src/client/java/com/example/NoSprintResettingConfig.java
+++ src/client/java/com/example/NoSprintResettingConfig.java
@@ -3,4 +3,12 @@ package com.example;
 public class NoSprintResettingConfig {
     public static boolean enabled = true;
     public static boolean disableWhileSneaking = true;
+
+    public static boolean isEnabled() {
+        return enabled;
+    }
+
+    public static void setEnabled(boolean value) {
+        enabled = value;
+    }
 }
