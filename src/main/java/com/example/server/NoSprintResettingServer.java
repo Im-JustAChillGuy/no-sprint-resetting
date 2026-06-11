5c50a569004c07fe0acd6558fe08de2f363d5c3e
Im-JustAChillGuy <justforfunbruh1234@gmail.com>
2026-06-11 16:32:43 +0530

    Update NoSprintResettingServer.java

--- src/main/java/com/example/server/NoSprintResettingServer.java
+++ src/main/java/com/example/server/NoSprintResettingServer.java
@@ -7,7 +7,7 @@ import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 
 public class NoSprintResettingServer implements ModInitializer {
 
