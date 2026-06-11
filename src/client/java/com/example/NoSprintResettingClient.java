7cb9fcd5031c632a233142547504dfdb6d43e2c6
Im-JustAChillGuy <justforfunbruh1234@gmail.com>
2026-06-11 16:28:51 +0530

    Update NoSprintResettingClient.java

--- src/client/java/com/example/NoSprintResettingClient.java
+++ src/client/java/com/example/NoSprintResettingClient.java
@@ -9,7 +9,7 @@ import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 
 public class NoSprintResettingClient implements ClientModInitializer {
 
